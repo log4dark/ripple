7e0ba4fe4239b75ebbfb8aa431a1efe254f8634d
dark <arthur.jeon@gmail.com>
2025-04-28 00:34:38 +0900

    Update requirements and settings.py

--- config/settings.py
+++ config/settings.py
@@ -4,10 +4,10 @@ from pydantic_settings import BaseSettings
 from functools import lru_cache
 
 class Settings(BaseSettings):
-    WHISPER_MODEL: str = "ct2-whisper-small-ko"
+    WHISPER_MODEL: str = "./whisper/ct2-whisper-small-ko-int8"
     WHISPER_DEVICE: str = "cpu"
     WHISPER_COMPUTE_TYPE: str = "int8"
-    WHISPER_MODEL_DIR: str = "./models"
+    WHISPER_MODEL_DIR: str = "./whisper"
 
     class Config:
         env_file = ".env"
